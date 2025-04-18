a22c6b7c2a3abac11350c7a8c0fab7a900269663
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-04-18 13:47:53 +0400

    setup convex and data schemas

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+// https://glad-griffon-33.clerk.accounts.dev
+
+export default {
+  providers: [
+    {
+      domain: "https://glad-griffon-33.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ],
+};
