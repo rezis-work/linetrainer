48fae2a57dc733087cad7dd4d514dfb69381d36a
DalaScript <nik.dalalishvili@gmail.com>
2025-04-29 14:32:25 +0400

    updated style

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -7,15 +7,15 @@ const ContactCard = ({type, title, info} : {type: string, title: string, info: s
 
     return (
         <div className="contact__card-wrapper w-full px-[12px] min-[576px]:w-[50%] min-[992px]:w-[33.3%]">
-            <div className="contact__card max-h-[167px] mt-[70px] p-[40px] relative flex flex-col items-center justify-center bg-[#ffffff0d] rounded-[10px]">
-                <div className="contact__card-icon bg-[#005de0] rounded-[50%] w-[80px] h-[80px] absolute top-[-40px] flex items-center justify-center">
+            <div className="contact__card max-h-[167px] mt-[70px] p-[40px] relative flex flex-col items-center justify-center shadow-sm bg-card/90 backdrop-blur-sm border border-border rounded-[16px]">
+                <div className="contact__card-icon shadow-sm bg-card/90 backdrop-blur-sm border border-border rounded-[50%] w-[80px] h-[80px] absolute top-[-40px] flex items-center justify-center">
                     
                     {type === "telephone" ? <PhoneCall size={40} color="#ffffff" /> 
                     : type === "email" ? <Mail size={40} color="#ffffff" /> 
                     : type === "location" ? <MapPin size={40} color="#ffffff"/> : null}
                 </div>
                 <h5 className="contact__car-title mt-[15px] text-[18px] text-white leading-[36px] font-bold">{title}</h5>
-                <p className="contact__card-info text-[18px] leading-[36px] text-[#005de0] font-bold decoration-[#7faeef] underline">{info}</p>
+                <p className="contact__card-info text-[18px] leading-[36px] text-[var(--primary)] font-bold decoration-[#7faeef] underline">{info}</p>
             </div>
         </div>
     );
--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -6,12 +6,12 @@ const ContactForm = () => {
         <div className="form-container my-[60px] px-[12px] flex flex-col items-center justify-center">
             <h2 className="form-container__title text-[28px]/[40px] font-semibold text-center min-[576px]:text-[30px]/[40px] min-[992px]:text-[36px]/[48px]">Get Support from our Team</h2>
             <form className="pt-[50px] w-full max-w-[516px] flex flex-col items-center justify-center gap-[20px] min-[768px]:max-w-[696px] min-[768px]:flex-row min-[768px]:flex-wrap min-[768px]:justify-between min-[992px]:max-w-[615px] min-[1200px]:max-w-[736px] min-[1400px]:max-w[856px]">
-                <input className="px-[25px] py-[10px] w-full bg-[#f6f7f9] h-[50px] font-semibold text-[#415674] placeholder:text-[#898c94] min-[768px]:w-[48%]" type="text" placeholder="Your Name" required/>
-                <input className="px-[25px] py-[10px] w-full bg-[#f6f7f9] h-[50px] font-semibold text-[#415674] placeholder:text-[#898c94] min-[768px]:w-[48%]" type="email" placeholder="Your Email" required/>
-                <input className="px-[25px] py-[10px] w-full bg-[#f6f7f9] h-[50px] font-semibold text-[#415674] placeholder:text-[#898c94] min-[768px]:w-[48%]" type="text" placeholder="Your Number" required/>
-                <input className="px-[25px] py-[10px] w-full bg-[#f6f7f9] h-[50px] font-semibold text-[#415674] placeholder:text-[#898c94] min-[768px]:w-[48%]" type="text" placeholder="subject" required/>
-                <textarea className="px-[25px] pt-[15px] pb-[10px] w-full bg-[#f6f7f9] h-[133px] font-semibold text-[#415674] placeholder:text-[#898c94]" placeholder="Write A Message" required></textarea>
-                <button className="bg-[#005de0] mx-auto mt-[15px] px-[30px] h-[45px] text-[16px]/[40px] font-bold rounded-[5px] " type="submit">Send Message</button>
+                <input className="px-[25px] py-[10px] w-full shadow-sm bg-card/90 backdrop-blur-sm border border-border rounded-[10px] h-[50px] font-semibold text-white placeholder:text-[#898c94] min-[768px]:w-[48%]" type="text" placeholder="Your Name" required/>
+                <input className="px-[25px] py-[10px] w-full shadow-sm bg-card/90 backdrop-blur-sm border border-border rounded-[10px] h-[50px] font-semibold text-white placeholder:text-[#898c94] min-[768px]:w-[48%]" type="email" placeholder="Your Email" required/>
+                <input className="px-[25px] py-[10px] w-full shadow-sm bg-card/90 backdrop-blur-sm border border-border rounded-[10px] h-[50px] font-semibold text-white placeholder:text-[#898c94] min-[768px]:w-[48%]" type="text" placeholder="Your Number" required/>
+                <input className="px-[25px] py-[10px] w-full shadow-sm bg-card/90 backdrop-blur-sm border border-border rounded-[10px] h-[50px] font-semibold text-white placeholder:text-[#898c94] min-[768px]:w-[48%]" type="text" placeholder="subject" required/>
+                <textarea className="px-[25px] pt-[15px] pb-[10px] w-full shadow-sm bg-card/90 backdrop-blur-sm border border-border rounded-[10px] h-[133px] font-semibold text-white placeholder:text-[#898c94]" placeholder="Write A Message" required></textarea>
+                <button className="mx-auto mt-[15px] px-[30px] h-[45px] text-[16px]/[40px] text-[var(--primary)] bg-background font-bold shadow-sm backdrop-blur-sm border border-ring rounded-md" type="submit">Send Message</button>
             </form>
         </div>
     );
